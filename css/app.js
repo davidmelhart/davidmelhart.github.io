3b1f0c7a564c1e5c994151bf69035d19bc8a3a6f
David Melhart <david.melhart@gmail.com>
2015-05-02 18:47:14 +0200

    V 0.1

---
+++ css/app.js
@@ -0,0 +1,16 @@
+// VIEWMODEL Start //
+
+function NavigatonViewModel() {
+	this.location = ko.observable("Location");
+};
+
+// VIEWMODEL End //
+
+ko.applyBindings(new NavigatonViewModel());
+
+
+// MODEL Start //
+
+
+
+// MODEL End //
---
+++ js/app.js
@@ -0,0 +1,51 @@
+// VIEWMODEL Start //
+
+function NavigatonViewModel() {
+	this.locationValue = ko.observable();
+	this.filter = ko.observable();
+	this.updateLocation = function() {
+		ko.computed(function(){
+			
+		}, this);	
+	}
+
+};
+
+
+
+// VIEWMODEL End //
+
+ko.applyBindings(new NavigatonViewModel());
+
+
+// MODEL Start //
+function mapRender() {
+	var center = new google.maps.LatLng(50, 8);
+		var mapOptions = {
+			zoom: 4,
+			center: center,
+			zoomControl: true,
+		    zoomControlOptions: {
+		        style: google.maps.ZoomControlStyle.LARGE,
+		        position: google.maps.ControlPosition.LEFT_BOTTOM
+		    },
+		    scaleControl: true,
+		    streetViewControl: true,
+		    streetViewControlOptions: {
+		        position: google.maps.ControlPosition.LEFT_BOTTOM
+		    },
+		    panControl: false,
+		    mapTypeControl: false,
+			MapTypeId: google.maps.MapTypeId.TERRAIN
+		};
+		var map = new google.maps.Map(document.getElementById("google-map"), mapOptions);
+		var naviForm = (document.getElementById("navi-form"));
+		naviForm.index = 1;
+    	map.controls[google.maps.ControlPosition.TOP_LEFT].push(naviForm);
+};
+
+window.addEventListener('load', mapRender);
+
+
+
+// MODEL End //
