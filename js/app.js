8f13d6ee90d1b6254307fbb61e4d8fcc9c429e9c
David Melhart <david.melhart@gmail.com>
2015-05-04 14:14:33 +0200

    V 0.5

--- js/app.js
+++ js/app.js
@@ -19,11 +19,102 @@ ko.applyBindings(new NavigatonViewModel());
 
 // MODEL Start //
 
-var map;
-var service;
-var infowindow;
+function initialize() {
 
-function mapRender() {
+  var markers = [];
+  var center = new google.maps.LatLng(50, 8);
+  var mapOptions = {
+			zoom: 5,
+			center: center,
+			zoomControl: true,
+		    zoomControlOptions: {
+		        style: google.maps.ZoomControlStyle.LARGE,
+		        position: google.maps.ControlPosition.LEFT_BOTTOM
+		    },
+		    scaleControl: true,
+		    streetViewControl: true,
+		    streetViewControlOptions: {
+		        position: google.maps.ControlPosition.LEFT_BOTTOM
+		    },
+		    panControl: false,
+		    mapTypeControl: false,
+			MapTypeId: google.maps.MapTypeId.TERRAIN
+		};
+	var map = new google.maps.Map(document.getElementById("google-map"), mapOptions);
+
+  	var defaultBounds = new google.maps.LatLngBounds(
+      	new google.maps.LatLng(-33.8902, 151.1759),
+    	new google.maps.LatLng(-33.8474, 151.2631));
+  	map.fitBounds(defaultBounds);
+	
+	// Setting custom navigation //
+
+	var naviForm = (document.getElementById("navi-form"));
+	naviForm.index = 1;
+	map.controls[google.maps.ControlPosition.TOP_LEFT].push(naviForm);
+
+ 	// Create the search box and link it to the UI element.
+  	var locationInput = /** @type {HTMLInputElement} */(
+    	document.getElementById("location"));
+ 	
+ 	// NOT USED ---> map.controls[google.maps.ControlPosition.TOP_LEFT].push(input);
+
+  	var searchBox = new google.maps.places.SearchBox(
+    /** @type {HTMLInputElement} */(locationInput));
+
+  // Listen for the event fired when the user selects an item from the
+  // pick list. Retrieve the matching places for that item.
+  	document.getElementById("submit-btn").addEventListener("click", function(){
+    	var places = searchBox.getPlaces();
+
+    	if (places.length == 0) {
+      		return;
+    	}
+    	for (var i = 0, marker; marker = markers[i]; i++) {
+      		marker.setMap(null);
+    	}
+
+    // For each place, get the icon, place name, and location.
+    	markers = [];
+    	var bounds = new google.maps.LatLngBounds();
+    	for (var i = 0, place; place = places[i]; i++) {
+	      	var image = {
+		        url: place.icon,
+		        size: new google.maps.Size(71, 71),
+		        origin: new google.maps.Point(0, 0),
+		        anchor: new google.maps.Point(17, 34),
+		        scaledSize: new google.maps.Size(25, 25)
+	      	};
+
+	      // Create a marker for each place.
+	      	var marker = new google.maps.Marker({
+		        map: map,
+		        icon: image,
+		        title: place.name,
+		        position: place.geometry.location
+	      	});
+
+	      	markers.push(marker);
+
+	      	bounds.extend(place.geometry.location);
+    	}
+
+    	map.fitBounds(bounds);
+  	});
+
+  // Bias the SearchBox results towards places that are within the bounds of the
+  // current map's viewport.
+  google.maps.event.addListener(map, 'bounds_changed', function() {
+    var bounds = map.getBounds();
+    searchBox.setBounds(bounds);
+  });
+}
+
+google.maps.event.addDomListener(window, 'load', initialize);
+
+
+
+/* function mapRender() {
 	var center = new google.maps.LatLng(50, 8);
 		var mapOptions = {
 			zoom: 5,
@@ -42,7 +133,7 @@ function mapRender() {
 		    mapTypeControl: false,
 			MapTypeId: google.maps.MapTypeId.TERRAIN
 		};
-		map = new google.maps.Map(document.getElementById("google-map"), mapOptions);
+		var map = new google.maps.Map(document.getElementById("google-map"), mapOptions);
 		
 		// Setting custom navigation //
 
@@ -53,8 +144,9 @@ function mapRender() {
 
 //Initializing the map//
 
-window.addEventListener('load', mapRender);
+google.maps.event.addDomListener(window, 'load', mapRender);
 
+*/
 
 
 
