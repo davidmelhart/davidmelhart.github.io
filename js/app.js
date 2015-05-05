1246d905106a9b0a41fc9138895b8be726c7b560
David Melhart <david.melhart@gmail.com>
2015-05-05 18:19:26 +0200

    V 0.7 App.js update

--- js/app.js
+++ js/app.js
@@ -19,135 +19,92 @@ ko.applyBindings(new NavigatonViewModel());
 
 // MODEL Start //
 
+
 function initialize() {
 
-  var markers = [];
-  var center = new google.maps.LatLng(50, 8);
-  var mapOptions = {
-			zoom: 5,
-			center: center,
-			zoomControl: true,
-		    zoomControlOptions: {
-		        style: google.maps.ZoomControlStyle.LARGE,
-		        position: google.maps.ControlPosition.LEFT_BOTTOM
-		    },
-		    scaleControl: true,
-		    streetViewControl: true,
-		    streetViewControlOptions: {
-		        position: google.maps.ControlPosition.LEFT_BOTTOM
-		    },
-		    panControl: false,
-		    mapTypeControl: false,
-			MapTypeId: google.maps.MapTypeId.TERRAIN
+	var mapOptions = {
+
+		zoomControl: true,
+		zoomControlOptions: {
+			style: google.maps.ZoomControlStyle.LARGE,
+			position: google.maps.ControlPosition.LEFT_BOTTOM
+		},
+		scaleControl: true,
+		streetViewControl: true,
+		streetViewControlOptions: {
+			position: google.maps.ControlPosition.LEFT_BOTTOM
+		},
+		panControl: false,
+		mapTypeControl: false,
+		MapTypeId: google.maps.MapTypeId.TERRAIN
 		};
+
 	var map = new google.maps.Map(document.getElementById("google-map"), mapOptions);
 
-  	var defaultBounds = new google.maps.LatLngBounds(
-      	new google.maps.LatLng(-33.8902, 151.1759),
-    	new google.maps.LatLng(-33.8474, 151.2631));
-  	map.fitBounds(defaultBounds);
-	
-	// Setting custom navigation //
+	var defaultBounds = new google.maps.LatLngBounds(
+    	new google.maps.LatLng(28.70, -127.50), 
+    	new google.maps.LatLng(48.85, -55.90)
+	);
+	map.fitBounds(defaultBounds);
 
+	// Setting custom navigation //
 	var naviForm = (document.getElementById("navi-form"));
 	naviForm.index = 1;
 	map.controls[google.maps.ControlPosition.TOP_LEFT].push(naviForm);
 
- 	// Create the search box and link it to the UI element.
-  	var locationInput = /** @type {HTMLInputElement} */(
-    	document.getElementById("location"));
- 	
- 	// NOT USED ---> map.controls[google.maps.ControlPosition.TOP_LEFT].push(input);
+	// Create the search box and link it to the UI element.
+	var locationInput = document.getElementById("location");
 
-  	var searchBox = new google.maps.places.SearchBox(
-    /** @type {HTMLInputElement} */(locationInput));
-
-  // Listen for the event fired when the user selects an item from the
-  // pick list. Retrieve the matching places for that item.
-  	document.getElementById("submit-btn").addEventListener("click", function(){
-    	var places = searchBox.getPlaces();
-
-    	if (places.length == 0) {
-      		return;
-    	}
-    	for (var i = 0, marker; marker = markers[i]; i++) {
-      		marker.setMap(null);
-    	}
-
-    // For each place, get the icon, place name, and location.
-    	markers = [];
-    	var bounds = new google.maps.LatLngBounds();
-    	for (var i = 0, place; place = places[i]; i++) {
-	      	var image = {
-		        url: place.icon,
-		        size: new google.maps.Size(71, 71),
-		        origin: new google.maps.Point(0, 0),
-		        anchor: new google.maps.Point(17, 34),
-		        scaledSize: new google.maps.Size(25, 25)
-	      	};
-
-	      // Create a marker for each place.
-	      	var marker = new google.maps.Marker({
-		        map: map,
-		        icon: image,
-		        title: place.name,
-		        position: place.geometry.location
-	      	});
-
-	      	markers.push(marker);
-
-	      	bounds.extend(place.geometry.location);
-    	}
-
-    	map.fitBounds(bounds);
-  	});
-
-  // Bias the SearchBox results towards places that are within the bounds of the
-  // current map's viewport.
-  google.maps.event.addListener(map, 'bounds_changed', function() {
-    var bounds = map.getBounds();
-    searchBox.setBounds(bounds);
-  });
-}
-
-google.maps.event.addDomListener(window, 'load', initialize);
+	var searchBox = new google.maps.places.SearchBox(locationInput);
 
 
+	markersArray = [];
+	function pinMaker(places){
+		// For each place creating a marker //
+		var marker = new google.maps.Marker({
+			map: map,
+			position: new google.maps.LatLng((places[place].geometry.location.A),(places[place].geometry.location.F)),
+			title: places[place].name
+		});
+		markersArray.push(marker);
 
-/* function mapRender() {
-	var center = new google.maps.LatLng(50, 8);
-		var mapOptions = {
-			zoom: 5,
-			center: center,
-			zoomControl: true,
-		    zoomControlOptions: {
-		        style: google.maps.ZoomControlStyle.LARGE,
-		        position: google.maps.ControlPosition.LEFT_BOTTOM
-		    },
-		    scaleControl: true,
-		    streetViewControl: true,
-		    streetViewControlOptions: {
-		        position: google.maps.ControlPosition.LEFT_BOTTOM
-		    },
-		    panControl: false,
-		    mapTypeControl: false,
-			MapTypeId: google.maps.MapTypeId.TERRAIN
-		};
-		var map = new google.maps.Map(document.getElementById("google-map"), mapOptions);
+		// For each place creating an infowindow //
 		
-		// Setting custom navigation //
+		var infoWindow = new google.maps.InfoWindow({
+			content: "<b>" + places[place].name + "</b></br>" + places[place].formatted_address
+			});
+			google.maps.event.addListener(marker, "click", function() {
+				infoWindow.open(map, marker);
+			});
+	}
+	// Adding a click event listener to open the info boxes //
 
-		var naviForm = (document.getElementById("navi-form"));
-		naviForm.index = 1;
-    	map.controls[google.maps.ControlPosition.TOP_LEFT].push(naviForm);
+	google.maps.event.addListener(searchBox, 'places_changed', function() {
+		// Clearing all previous requests //
+		clearOverlays();
+		
+		var bounds = new google.maps.LatLngBounds();
+		var places = searchBox.getPlaces();
+		console.log(places);
+		
+	// Calling pinMaker for each place //		
+		for (place in places) {
+			pinMaker(places);
+		// Setting the map to fit into the bounds defined by the marker pins //
+		bounds.extend(places[place].geometry.location);
+		map.fitBounds(bounds);	
+		}
+
+	});
+// Following code is snatched from http://stackoverflow.com/questions/1544739/google-maps-api-v3-how-to-remove-all-markers
+// It clears all previous pins when called.
+	function clearOverlays() {
+	  for (var i = 0; i < markersArray.length; i++ ) {
+	    markersArray[i].setMap(null);
+	  }
+	  markersArray.length = 0;
+	}
+
 };
 
-//Initializing the map//
-
-google.maps.event.addDomListener(window, 'load', mapRender);
-
-*/
-
-
-
-// MODEL End //
+google.maps.event.addDomListener(window, 'load', initialize);
